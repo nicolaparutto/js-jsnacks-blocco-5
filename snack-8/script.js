2409e88080028c8a4520f743cc85f829055760e2
nicolaparutto <nicolaparutto2003@gmail.com>
2024-12-02 16:30:25 +0100

    feat: 8 snack completed all exercises completed

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,6 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+const classOfMarco = students.find(student => student.name === 'Marco Lanci').class;
+
+console.log(classOfMarco); 
