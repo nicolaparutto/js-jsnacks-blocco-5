5ef72c9e6bf165bc0ea8cdf36f7036ef0c953235
nicolaparutto <nicolaparutto2003@gmail.com>
2024-12-02 16:01:53 +0100

    feat: 4 snack completed

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,8 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const postsNames = posts.map(author =>{
+  return author.author;
+});
+console.log(postsNames);
