1aba674cf05bfe1f999e33b7c1a4808dcab6fa0a
nicolaparutto <nicolaparutto2003@gmail.com>
2024-12-02 16:10:04 +0100

    feat: 6 snack completed

--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,12 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+const zucchineLunghe = zucchine.filter(zucchina =>{
+  return zucchina.length >= 15;
+});
+console.log(zucchineLunghe);
+
+const zucchineRimanenti = zucchine.filter(zucchina =>{
+  return zucchina.length <= 15;
+});
+console.log(zucchineRimanenti);
